e0a496369d4938469fd425b054ad8909c63005cb
luis-luis <luisdebortoli7@gmail.com>
2025-01-29 15:38:59 -0300

    Testando CSS

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -18,6 +18,7 @@
     <!-- Principal CSS do Bootstrap -->
     <link href="css/bootstrap.css" rel="stylesheet">
 
+
     <link href="css/app.css" rel="stylesheet">
 
     <link href="css/stylehome.css" rel="stylesheet">
@@ -28,6 +29,7 @@
     <!-- Estilos customizados para outras paginas -->
     <link href="css/style.css" rel="stylesheet">
 
+    <!-- <link href="css/custom.css" rel="stylesheet"> Sobrescreve estilos -->
 
     <!-- Custom styles for this template -->
     <link href="pricing.css" rel="stylesheet">
