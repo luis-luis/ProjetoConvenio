f844fa1eee6acc441f22c163e28fefb21fd19c2d
luis-luis <luisdebortoli7@gmail.com>
2025-02-04 15:53:33 -0300

    atualizações htaccess e public

--- resources/views/layout/main2.blade.php
+++ resources/views/layout/main2.blade.php
@@ -6,7 +6,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <link rel="icon" href="{{ asset('veterinario.png')}}" type="image/png">
+  <link rel="icon" href="{{ asset('images/veterinario.png')}}" type="image/png">
 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 
