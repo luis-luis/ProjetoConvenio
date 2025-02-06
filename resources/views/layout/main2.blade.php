1aaa354703302e532475b455af7a9e57cacb8357
luis-luis <luisdebortoli7@gmail.com>
2025-02-06 11:59:37 -0300

    Update main2.blade.php

--- resources/views/layout/main2.blade.php
+++ resources/views/layout/main2.blade.php
@@ -171,7 +171,7 @@
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
   <script src="js/google-map.js"></script>
   <script src="js/main.js"></script>
-  <script src="{{ asset('js/scroll.js') }}"></script>
+  <script src="js/scroll.js"></script>
 
 
 
