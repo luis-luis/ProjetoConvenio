d1d8642a4b9fe95a5d2ddc8574bbd64448dedce9
luis-luis <luisdebortoli7@gmail.com>
2025-02-12 18:45:28 -0300

    Update main2.blade.php

--- resources/views/layout/main2.blade.php
+++ resources/views/layout/main2.blade.php
@@ -191,7 +191,7 @@
   <script src="js/jquery.timepicker.min.js"></script>
   <script src="js/owl.carousel.min.js"></script>
   <script src="js/jquery.magnific-popup.min.js"></script>
-  <script src="js/scrollax.min.js"></script>
+  <!-- <script src="js/scrollax.min.js"></script> -->
 
   <!-- Google Maps -->
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
