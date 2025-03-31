048d70488ae6b0b3358c07ff075450128912de44
luis-luis <luisdebortoli7@gmail.com>
2025-03-31 11:26:54 -0300

    update

--- resources/views/layout/main2.blade.php
+++ resources/views/layout/main2.blade.php
@@ -18,8 +18,6 @@
 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 
-  <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
-
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
   <link rel="stylesheet" href="css/animate.css">
