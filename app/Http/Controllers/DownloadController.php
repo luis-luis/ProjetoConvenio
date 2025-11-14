17c966e70dafcdc24708d21767b6da533790e17b
luis-luis <luisdebortoli7@gmail.com>
2025-11-14 11:33:07 -0300

    alteração download

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -8,8 +8,14 @@ use Illuminate\Support\Facades\Storage;
 
 class DownloadController extends Controller
 {
+    // public function downloadFile($filename){
+    //     $filepath = 'app/'.$filename;
+    //     return response()->download(storage_path($filepath), $filename);
+    // }
+
     public function downloadFile($filename){
-        $filepath = 'app/'.$filename;
-        return response()->download(storage_path($filepath), $filename);
-    }
+    return response()->download(public_path("downloads/{$filename}"));
 }
+
+}
+
