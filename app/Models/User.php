c0fd8add4a34a03a92d2b70f520a2bdfcd78076b
luis-luis <luisdebortoli7@gmail.com>
2025-12-03 08:52:40 -0300

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,8 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    protected $connection = 'pgsql_render';
+
     /**
      * The attributes that are mass assignable.
      *
